cc9a9c783d50d317ccea6f293de9d49813c0c76d
ilchovski98 <ilchovski98@gmail.com>
2022-04-22 17:00:03 +0300

    Throwing exceptions

--- exceptions/src/com/codingwithnikola/exceptions/ExceptionsDemo.java
+++ exceptions/src/com/codingwithnikola/exceptions/ExceptionsDemo.java
@@ -1,31 +1,14 @@
 package com.codingwithnikola.exceptions;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-
+        var account = new Account();
         try {
-            reader = new FileReader("file.txt");
-            var value = reader.read();
-        }
-        catch (IOException e) {
-            System.out.println("Could not read data.");
-        }
-        finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            account.deposit(1);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
