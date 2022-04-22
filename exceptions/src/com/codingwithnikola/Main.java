e5ce4c96b291cbee1503942d604970a7acee0bb1
ilchovski98 <ilchovski98@gmail.com>
2022-04-22 10:57:17 +0300

    Ugly error handling and releasing external resources

---
+++ exceptions/src/com/codingwithnikola/Main.java
@@ -0,0 +1,9 @@
+package com.codingwithnikola;
+
+import com.codingwithnikola.exceptions.ExceptionsDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        ExceptionsDemo.show();
+    }
+}
