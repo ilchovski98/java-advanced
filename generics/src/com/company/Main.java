a8e1abca37d2f89bf443d8d544272d5d7e0ed3ac
ilchovski98 <ilchovski98@gmail.com>
2022-04-23 13:10:43 +0300

    Generic list solution

--- generics/src/com/company/Main.java
+++ generics/src/com/company/Main.java
@@ -2,13 +2,18 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new List();
-        list.add(21);
-        list.add(Integer.valueOf(21));
-        list.add("New string");
-        list.add(new User());
+//        var list = new List();
+//        list.add(21);
+//        list.add(Integer.valueOf(21));
+//        list.add("New string");
+//        list.add(new User());
+//
+//        int number = (int)list.get(1);
+//        System.out.println(number);
 
-        int number = (int)list.get(1);
-        System.out.println(number);
+        var list = new GenericList<String>();
+        list.add("Yeah");
+        String string = list.get(0);
+        System.out.println(string);
     }
 }
