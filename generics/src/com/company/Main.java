bd7af1d980bec93cb6bb175338f3c2a2d5e62216
ilchovski98 <ilchovski98@gmail.com>
2022-04-23 12:55:39 +0300

    Poor solution to universal list class that works with all types

--- generics/src/com/company/Main.java
+++ generics/src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         var list = new List();
         list.add(21);
+        list.add(Integer.valueOf(21));
+        list.add("New string");
+        list.add(new User());
+
+        int number = (int)list.get(1);
+        System.out.println(number);
     }
 }
