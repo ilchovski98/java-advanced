4bf366d6d475701ca83f0fd86e009cbc6bda5d64
ilchovski98 <ilchovski98@gmail.com>
2022-04-23 12:50:44 +0300

    Add int list

--- exceptions/src/com/codingwithnikola/Main.java
+++ exceptions/src/com/codingwithnikola/Main.java
@@ -2,8 +2,14 @@ package com.codingwithnikola;
 
 import com.codingwithnikola.exceptions.ExceptionsDemo;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        ExceptionsDemo.show();
+        try {
+            ExceptionsDemo.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ generics/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        var list = new List();
+        list.add(21);
+    }
+}
