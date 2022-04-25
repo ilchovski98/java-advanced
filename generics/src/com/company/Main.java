78cb3a0a90a9e47e8e1dac03b0e72044942003fb
ilchovski98 <ilchovski98@gmail.com>
2022-04-25 10:37:35 +0300

    Comparable Interface

--- generics/src/com/company/Main.java
+++ generics/src/com/company/Main.java
@@ -2,24 +2,14 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-//        var list = new List();
-//        list.add(21);
-//        list.add(Integer.valueOf(21));
-//        list.add("New string");
-//        list.add(new User());
-//
-//        int number = (int)list.get(1);
-//        System.out.println(number);
+        var user1 = new User(10);
+        var user2 = new User(20);
 
-        var list = new GenericList<String>();
-        list.add("Yeah");
-        String string = list.get(0);
-        System.out.println(string);
-
-//        var intList = new GenericList<Integer>();
-        GenericList<Integer> intList = new GenericList<>();
-        intList.add(1); // Boxing
-        int ints = intList.get(0); // Unboxing
-        System.out.println(ints);
+        if (user1.compareTo(user2) < 0)
+            System.out.println("User 1 < User 2");
+        else if (user1.compareTo(user2) == 0)
+            System.out.println("User 1 == User 2");
+        else
+            System.out.println("User 1 > User 2");
     }
 }
--- generics/src/com/company/User.java
+++ generics/src/com/company/User.java
@@ -1,5 +1,22 @@
 package com.company;
 
-public class User {
+public class User implements Comparable<User> {
+    private int points;
 
+    public User(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public int compareTo(User other) {
+        return points - other.points;
+
+//        if (points < o.points)
+//            return -1;
+//
+//        if (points == o.points)
+//            return 0;
+//
+//        return -1;
+    }
 }
