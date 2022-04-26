ee94ce2d90906935a26ac7c543b717f2bea31b58
ilchovski98 <ilchovski98@gmail.com>
2022-04-26 13:40:18 +0300

    Multiple type parameters

--- generics/src/com/company/Main.java
+++ generics/src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import jdk.jshell.execution.Util;
+
 public class Main {
     public static void main(String[] args) {
         var max = Utils.max(new User(10), new User(20));
         System.out.println(max);
+
+        Utils.print(21, "Nikola");
     }
 }
