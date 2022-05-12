b7f89b4d21d09c4bbe541ba6c47066756d70b625
ilchovski98 <ilchovski98@gmail.com>
2022-05-12 20:47:04 +0300

    Comparing two collections

--- collections/src/collections/CollectionsDemo.java
+++ collections/src/collections/CollectionsDemo.java
@@ -7,19 +7,9 @@ import java.util.Collections;
 public class CollectionsDemo {
     public static void show() {
         Collection<String> collection = new ArrayList<>();
-        collection.add("a");
-        collection.add("b");
-        collection.add("c");
-        for (var item : collection) {
-            System.out.println(item);
-        }
-
-        System.out.println(collection);
-
-        var containsA = collection.contains("a");
-        System.out.println("containsA " + containsA);
-        System.out.println(collection.isEmpty());
-        var objectArray = collection.toArray();
-        var stringArray = collection.toArray(new String[0]);
+        Collection<String> other = new ArrayList<>();
+        other.addAll(collection);
+        System.out.println(collection == other);
+        System.out.println(collection.equals(other));
     }
 }
