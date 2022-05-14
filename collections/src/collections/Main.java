33fa8c702693e0fd0b9085d5c4d3cf499e6b0df2
ilchovski98 <ilchovski98@gmail.com>
2022-05-14 13:52:34 +0300

    Sorting Customer (objects) by their names

--- collections/src/collections/Main.java
+++ collections/src/collections/Main.java
@@ -1,7 +1,17 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        ListDemo.show();
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("b"));
+        customers.add(new Customer("a"));
+        customers.add(new Customer("c"));
+        System.out.println(customers);
+        Collections.sort(customers);
+        System.out.println(customers);
     }
 }
