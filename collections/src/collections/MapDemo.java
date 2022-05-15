c45ee67bda3bdb86c75f7538d10900b73136ec47
ilchovski98 <ilchovski98@gmail.com>
2022-05-15 14:59:45 +0300

    Map interface basiscs

--- collections/src/collections/MapDemo.java
+++ collections/src/collections/MapDemo.java
@@ -1,22 +1,39 @@
 package collections;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapDemo {
     public static void show() {
-        // Find a customer by email (customers number could be millions)
-        // Bad example
-        // O(n) - n = number of customers
-//        List<Customer> customers = new ArrayList<>();
-//        for (var customer : customers)
-//            if (customer.getEmail() == "e1")
-//                System.out.println("Found!");
+        var c1 = new Customer("a", "e1");
+        var c2 = new Customer("b", "e2");
 
-        // HashTables in different languages (they can access specific customer instantly)
-        // Java: Maps
-        // C#: Dictionary
-        // Python: Dictionary
-        // Javascript: Objects - var person = { name: "a" }
+        Map<String, Customer> map = new HashMap<>();
+
+        map.put(c1.getEmail(), c1);
+        map.put(c2.getEmail(), c2);
+
+        var unknown = new Customer("Unknown", "");
+        var customer = map.get("e2");
+        var customer2 = map.getOrDefault("e10", unknown);
+        var exists = map.containsKey("e10");
+        var replaceCustomer = map.replace("e1", new Customer("a++", "e1"));
+
+        System.out.println("customer " + customer);
+        System.out.println("customer2 " + customer2);
+        System.out.println("exists " + exists);
+        System.out.println("replaceCustomer " + replaceCustomer);
+        System.out.println("map " + map);
+
+        for (var key : map.keySet())
+            System.out.println(key);
+
+        for (var entry : map.entrySet())
+            System.out.println(entry);
+//            System.out.println(entry.getKey());
+
+        for (var currentCustomer : map.values())
+            System.out.println(currentCustomer);
+
     }
 }
