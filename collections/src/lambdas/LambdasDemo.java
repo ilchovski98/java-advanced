6114c1eca11d429d3b42fdb1192b5abe7a868aec
ilchovski98 <ilchovski98@gmail.com>
2022-05-15 16:16:04 +0300

    Method reference - instance methods

--- collections/src/lambdas/LambdasDemo.java
+++ collections/src/lambdas/LambdasDemo.java
@@ -1,15 +1,14 @@
 package lambdas;
 
 public class LambdasDemo {
-    public static void print(String message) {
+    public void print(String message) {
         System.out.println(message);
     }
 
     public void show() {
-//        greet(message -> System.out.println(message));
-//        greet(System.out::println);
-//        greet(message -> print(message));
-        greet(LambdasDemo::print);
+        var demo = new LambdasDemo();
+        greet(message -> demo.print(message));
+        greet(demo::print);
     }
 
     public static void greet(Printer printer) {
