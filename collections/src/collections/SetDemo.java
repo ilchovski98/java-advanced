4f06f3533d0009d51bccfbc9fc93501b030bbf45
ilchovski98 <ilchovski98@gmail.com>
2022-05-15 13:44:51 +0300

    Sets - useful methods

--- collections/src/collections/SetDemo.java
+++ collections/src/collections/SetDemo.java
@@ -4,16 +4,19 @@ import java.util.*;
 
 public class SetDemo {
     public static void show() {
-//        Set<String> set = new HashSet<>();
-//        set.add("sky");
-//        set.add("is");
-//        set.add("blue");
-//        set.add("blue");
-//        System.out.println(set);
-        Collection<String> collection = new ArrayList<>();
-        Collections.addAll(collection, "a", "b", "c", "c");
-        System.out.println("collection" + collection);
-        Set<String> set = new HashSet<>(collection);
-        System.out.println("Set " + set);
+        Set<String> set1 = new HashSet<>(Arrays.asList("a", "b", "c"));
+        Set<String> set2 = new HashSet<>(Arrays.asList("b", "c", "d"));
+
+        // Union operation - combines both sets unique values
+//        set1.addAll(set2);
+//        System.out.println(set1);
+
+        // Intersection - returns the common values in both sets
+//        set1.retainAll(set2);
+//        System.out.println(set1);
+
+        // Difference - returns the item that we have in set1 but not in set2
+        set1.removeAll(set2);
+        System.out.println(set1);
     }
 }
