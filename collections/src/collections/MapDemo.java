620219d48d05bf620a448782ec3f08651b4e6a31
ilchovski98 <ilchovski98@gmail.com>
2022-05-15 13:54:26 +0300

    Hash table naming across multiple programming languages

---
+++ collections/src/collections/MapDemo.java
@@ -0,0 +1,22 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapDemo {
+    public static void show() {
+        // Find a customer by email (customers number could be millions)
+        // Bad example
+        // O(n) - n = number of customers
+//        List<Customer> customers = new ArrayList<>();
+//        for (var customer : customers)
+//            if (customer.getEmail() == "e1")
+//                System.out.println("Found!");
+
+        // HashTables in different languages (they can access specific customer instantly)
+        // Java: Maps
+        // C#: Dictionary
+        // Python: Dictionary
+        // Javascript: Objects - var person = { name: "a" }
+    }
+}
